b8c880240bcfa619b88365608a4536781f8934d6
slawekSk13 <slawomir.skrzeczynski@gmail.com>
2021-07-06 12:53:12 +0200

    playing with themes

--- src/styleguide/Wrapper.js
+++ src/styleguide/Wrapper.js
@@ -3,12 +3,24 @@ import {ThemeProvider} from 'styled-components';
 
  const Wrapper = (props) => {
      const theme = {
-         color: '#E5E5E5',
-         backgroundColor: '#CD1719',
-         hoverColor: '#951B81'
+         default: {
+             color: '#E5E5E5',
+             backgroundColor: '#951B81',
+             hoverColor: '#CD1719'
+         },
+         reversed: {
+             color: '#E5E5E5',
+             backgroundColor: '#CD1719',
+             hoverColor: '#951B81'
+         },
+         dark: {
+             color: '#951B81',
+             backgroundColor: '#E5E5E5',
+             hoverColor: '#E5E5E5'
+         }
      }
      return(
-         <ThemeProvider theme={theme}>
+         <ThemeProvider theme={theme.default}>
              {props.children}
          </ThemeProvider>
      );
